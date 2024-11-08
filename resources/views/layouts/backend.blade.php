d73fb2697dfc9b9e13d88de2ad54e0445256ce7d
Jefferson Ortega <jortega@DOMAIN.LOCAL>
2024-11-07 21:09:40 -0500

    pestaña con nombre y barra lateral con icono

--- resources/views/layouts/backend.blade.php
+++ resources/views/layouts/backend.blade.php
@@ -6,25 +6,12 @@
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <meta name="viewport" content="width=device-width,initial-scale=1.0">
 
-    <title>Codebase - Bootstrap 5 Admin Template &amp; UI Framework</title>
-
-    <meta name="description" content="Codebase - Bootstrap 5 Admin Template &amp; UI Framework created by pixelcave">
-    <meta name="author" content="pixelcave">
-    <meta name="robots" content="index, follow">
-
-    <!-- Open Graph Meta -->
-    <meta property="og:title" content="Codebase - Bootstrap 5 Admin Template &amp; UI Framework">
-    <meta property="og:site_name" content="Codebase">
-    <meta property="og:description"
-        content="Codebase - Bootstrap 5 Admin Template &amp; UI Framework created by pixelcave">
-    <meta property="og:type" content="website">
-    <meta property="og:url" content="">
-    <meta property="og:image" content="">
+    <title>Loguin</title>
 
     <!-- Icons -->
-    <link rel="shortcut icon" href="{{ asset('media/favicons/favicon.png') }}">
+{{--     <link rel="shortcut icon" href="{{ asset('media/favicons/favicon.png') }}">
     <link rel="icon" sizes="192x192" type="image/png" href="{{ asset('media/favicons/favicon-192x192.png') }}">
-    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('media/favicons/apple-touch-icon-180x180.png') }}">
+    <link rel="apple-touch-icon" sizes="180x180" href="{{ asset('media/favicons/apple-touch-icon-180x180.png') }}"> --}}
 
     <!-- Modules -->
     @yield('css')
@@ -137,12 +124,12 @@
                     <!-- Logo -->
                     <div>
                         <span class="smini-visible fw-bold tracking-wide fs-lg">
-                            c<span class="text-primary">b</span>
+                            L<span class="text-primary">g</span>
                         </span>
                         <a class="link-fx fw-bold tracking-wide mx-auto" href="/">
                             <span class="smini-hidden">
-                                <i class="fa fa-fire text-primary"></i>
-                                <span class="fs-4 text-dual">code</span><span class="fs-4 text-primary">base</span>
+                                <i class="fa fa-users-gear text-primary"></i>
+                                <span class="fs-4 text-dual">Log</span><span class="fs-4 text-primary">uin</span>
                             </span>
                         </a>
                     </div>
